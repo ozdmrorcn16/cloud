4eb336bc810f0baffe2acc53812a7b2ac9d5f05e
ozdmrorcn16 <ozdmrorcn16@gmail.com>
2026-08-13 10:00:14 +0300

    mobil: Use official AsyncStorage mock instead of hand-rolled stub

--- mobil/jest.setup.js
+++ mobil/jest.setup.js
@@ -6,15 +6,6 @@ process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY =
   process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ||
   'placeholder-anon-key-pending-task-2'
 
-jest.mock('@react-native-async-storage/async-storage', () => {
-  const mockAsyncStorage = {
-    getItem: jest.fn(() => Promise.resolve(null)),
-    setItem: jest.fn(() => Promise.resolve()),
-    removeItem: jest.fn(() => Promise.resolve()),
-    multiGet: jest.fn(() => Promise.resolve([])),
-    multiSet: jest.fn(() => Promise.resolve()),
-    multiRemove: jest.fn(() => Promise.resolve()),
-    clear: jest.fn(() => Promise.resolve()),
-  }
-  return mockAsyncStorage
-})
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+)
